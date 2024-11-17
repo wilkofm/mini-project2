5736c0199867763acf33cf4edf726e443d79268c
wilkofm <maxferriswilkinson@gmail.com>
2024-11-17 16:45:33 +1100

    Committing early work on dark-mode

---
+++ vite-project/context/MyThemeContext.jsx
@@ -0,0 +1,9 @@
+import React, { createContext, useState, useContext } from "react";
+
+export const themes = {
+  light: {
+    foreground: "#f5f7fa",
+    background: "#fefefe",
+    text: "#000000",
+  },
+};
